5ab9b7a21c37d5f00109e58f214ee585a63ae07a
bibudha <bibudha.sahoo@gmail.com>
2013-09-02 12:15:01 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -185,7 +185,8 @@ var DescriptionVoice,EmailVoice,subjectVoice,mobileBackgroundVoice; // variables
   	var nav_url="http://docs.google.com/viewer?url="+ pdfUrl ;
   	//alert(nav_url);
   	$("#header_title").html(pdfTitle);
-  	window.open(nav_url, '_system');
+  	alert(nav_url);
+  	window.open(nav_url,'_system');
   	//window.location.href=nav_url;
   	//navigator.load.url(pdfUrl);
   	//var ref = window.open(pdfUrl, '_blank');
